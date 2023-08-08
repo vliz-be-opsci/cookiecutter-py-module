7437904bc743e79ea38271f09853bed705bcf972
JotaFan <jotaflame@gmail.com>
2023-08-08 09:52:59 +0200

    fix: remove git and venv from post gen script

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,17 +1,4 @@
 import subprocess
 
-# setup git
-subprocess.call(['git', 'init'])
-subprocess.call(['git', 'config', 'user.name', '{{cookiecutter.full_name}}'])
-subprocess.call(['git', 'config', 'user.email', '{{cookiecutter.email}}'])
-subprocess.call(['git', 'add', '.'])
-subprocess.call(['git', 'commit', '-m', 'Initial commit'])
-
-remote = '{{cookiecutter.git_remote}}'
-
-if remote != 'Git remote (if known)':
-    subprocess.call(['git', 'remote', 'add', 'origin', remote])
-    subprocess.call(['git', 'push', '-u', 'origin', 'master'])
-
 # setup venv
-subprocess.call(['virtualenv', 'venv', '-p', 'python3'])
+subprocess.call(['make', 'init-dev'])
